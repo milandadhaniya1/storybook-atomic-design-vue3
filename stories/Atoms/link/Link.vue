95f1c23a61e42952c4775ce49781a32415d1a3dd
milan_carsaver <simform.milan.dadhaniya@carsaver.com>
2023-08-29 17:49:22 +0530

    Atoms added

---
+++ stories/Atoms/link/Link.vue
@@ -0,0 +1,27 @@
+<template>
+  <a :href="url" :title="altTitle?altTitle:title" :target="target" :class="classes">{{ title }}</a>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  altTitle: {
+    type: String,
+    required: false,
+  },
+  target: {
+    type: String,
+    required: false,
+    default: "",
+  }
+});
+
+const classes = "text-blue-600 underline dark:text-blue-500 hover:no-underline";
+</script>
